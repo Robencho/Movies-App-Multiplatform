7bc839ee9fe04b867c5805fb74f939c548fe1064
Robencho <rubian.llanos.7@gmail.com>
2025-03-20 13:09:00 -0500

    [FEATURE] Implement first unit testing and base continues integration.

--- lib/data/models/movie_model.g.dart
+++ lib/data/models/movie_model.g.dart
@@ -9,24 +9,23 @@ part of 'movie_model.dart';
 MovieModel _$MovieModelFromJson(Map<String, dynamic> json) => MovieModel(
       adult: json['adult'] as bool,
       backdropPath: json['backdrop_path'] as String?,
-      genreIds: (json['genre_ids'] as List<dynamic>?)
-              ?.map((e) => (e as num).toInt())
-              .toList() ??
-          [],
+      genreIds: (json['genre_ids'] as List<dynamic>)
+          .map((e) => (e as num).toInt())
+          .toList(),
       id: (json['id'] as num).toInt(),
-      originalLanguage: json['original_language'] as String? ?? '',
-      originalTitle: json['original_title'] as String? ?? '',
-      overview: json['overview'] as String? ?? '',
+      originalLanguage: json['original_language'] as String,
+      originalTitle: json['original_title'] as String,
+      overview: json['overview'] as String?,
       popularity: json['popularity'] == null
           ? 0
           : MovieModel._parseDouble(json['popularity']),
       posterPath: json['poster_path'] as String?,
       releaseDate: json['release_date'] as String?,
-      title: json['title'] as String? ?? '',
-      video: json['video'] as bool? ?? false,
-      voteAverage: (json['vote_average'] as num?)?.toDouble() ?? 0,
-      voteCount: (json['vote_count'] as num?)?.toInt() ?? 0,
-      totalPages: (json['total_pages'] as num?)?.toInt() ?? 0,
+      title: json['title'] as String,
+      video: json['video'] as bool,
+      voteAverage: (json['vote_average'] as num).toDouble(),
+      voteCount: (json['vote_count'] as num).toInt(),
+      totalPages: (json['total_pages'] as num?)?.toInt(),
     );
 
 Map<String, dynamic> _$MovieModelToJson(MovieModel instance) =>
